85152f66f2c321d5b31df04246c0820b03e12b1c
Jun Li <junchenli423@gmail.com>
2025-10-02 15:05:32 -0400

    Added all endpoints, tested them using postman. Need to integrate to React front-end then add JWT later

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -29,3 +29,27 @@ export async function FindTaskById(req,res,next){
         res.status(500).json({ error: err.message });
   }
 }
+
+export async function UpdateTask(req,res,next){
+    try {
+        const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body);
+        if (!updatedTask) {
+            return res.status(404).json({ error: "Task not found" });
+        }
+        res.json(updatedTask);
+  } catch (err) {
+        res.status(400).json({ error: err.message });
+  }
+}
+
+export async function DeleteTask(req,res,next){
+    try {
+        const deletedTask = await Task.findByIdAndDelete(req.params.id);
+        if (!deletedTask) {
+            return res.status(404).json({ error: "Task not found" });
+        }
+        res.json({ message: "Task deleted" });
+  } catch (err) {
+        res.status(500).json({ error: err.message });
+  }
+}
--- backend/src/routes/tasksRouter.js
+++ backend/src/routes/tasksRouter.js
@@ -3,6 +3,8 @@ import '../models/Task.model.js'
 import { CreateTask } from "../controllers/tasksController.js";
 import { GetAllTasks } from "../controllers/tasksController.js";
 import { FindTaskById } from "../controllers/tasksController.js";
+import { DeleteTask } from "../controllers/tasksController.js";
+import { UpdateTask } from "../controllers/tasksController.js"; // turn this all to one line later
 
 const router = express.Router(); // create router obj
 
@@ -10,7 +12,14 @@ const router = express.Router(); // create router obj
 router.post('/create', CreateTask);
 
 /*Get endpoints (READ)*/
-router.get('/all', () => {console.log('hello1')});
+router.get('/all', GetAllTasks);
+router.get('/task/:id', FindTaskById);
+
+/*Put endpoints (UPDATE)*/
+router.put('/update/:id', UpdateTask);
+
+/*Delete endpoints (DELETE)*/ 
+router.delete('/delete/:id', DeleteTask);
 
 
 export default router;
